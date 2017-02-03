893c5101e6e5eac6d3426f4e9dc21ca5cb1f541c
Jeff-Russ <jeffreylynnruss@gmail.com>
2017-02-03 17:13:22 -0500

    clean up file structure to make it less flat

--- functions.php
+++ functions.php
@@ -23,11 +23,11 @@ if ( ! function_exists( '_s_setup' ) ) :
 function _s_setup() {
 	/*
 	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
+	 * Translations can be filed in the inc/languages/ directory.
 	 * If you're building a theme based on _s, use a find and replace
 	 * to change '_s' to the name of your theme in all the template files.
 	 */
-	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+	load_theme_textdomain( '_s', get_template_directory() . '/inc/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
@@ -112,9 +112,9 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -188,7 +188,7 @@ if ( !function_exists('enqueue_bootstrap_js_edit') ){
 	add_action('wp_enqueue_scripts', 'enqueue_bootstrap_js_edit');
 	function enqueue_bootstrap_js_edit(){
 		wp_enqueue_script('bs-js', get_stylesheet_directory_uri()
-			.'/bootstraplet.js',array('jquery'), '', true );
+			.'/assets/js/bootstraplet.js',array('jquery'), '', true );
 	}
 }
 
@@ -198,7 +198,7 @@ if ( !function_exists('enqueue_bootstrap_css_edit') ){
 	add_action('wp_enqueue_scripts', 'enqueue_bootstrap_css_edit');
 	function enqueue_bootstrap_css_edit(){
 		wp_enqueue_style('bs-css', get_stylesheet_directory_uri()
-			. '/bootstraplet.css');
+			. '/assets/css/bootstraplet.css');
 	}
 }
 if ( !function_exists('enqueue_bootstrap_css_cdn') ){
